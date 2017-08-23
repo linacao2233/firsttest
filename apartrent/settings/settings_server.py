2873c3f3b7fcb6a21bdfe416fc79488cc18a2a78
lcao <lncao6@gmail.com>
2017-08-23 11:26:48 +0300

    library path

--- apartrent/settings/settings_server.py
+++ apartrent/settings/settings_server.py
@@ -210,4 +210,8 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 
 
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so'
+#GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so'
+from os import environ
+GEOS_LIBRARY_PATH = "{}/libgeos_c.so".format(environ.get('GEOS_LIBRARY_PATH'))
+GDAL_LIBRARY_PATH = "{}/libgdal.so".format(environ.get('GDAL_LIBRARY_PATH'))
+PROJ4_LIBRARY_PATH = "{}/libproj.so".format(environ.get('PROJ4_LIBRARY_PATH'))
